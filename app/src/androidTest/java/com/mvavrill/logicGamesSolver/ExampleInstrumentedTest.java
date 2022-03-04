3b3e209d5811fc034e85ba59cce215991d34a59b
Mathieu Vavrille <mathieu.vavrille@gmail.com>
2022-03-04 18:30:31 +0100

    First commit

--- app/src/androidTest/java/com/mvavrill/logicGamesSolver/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mvavrill/logicGamesSolver/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.mvavrill.sudokusolver", appContext.getPackageName());
+        assertEquals("com.mvavrill.logicGamesSolver", appContext.getPackageName());
     }
 }
